7240572075ab83cee27c5270bf3f999f99cbc9f7
github username <github email address>
2018-07-11 16:13:22 -0400

    Entries_Controller CRUD actions complete

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,4 +1,40 @@
 class Api::V1::EntriesController < ApplicationController
+  before_action :find_entry
+  def index
+    @entries = Entry.all
+    render json: @entries
+  end
 
-  
+  def update
+    @entry.update(entry_params)
+    if @entry.save
+      render json: @entry, status: :accepted
+    else
+      render json: {errors: @entry.errors.full_messages}, status: :unproccesible_entity
+    end
+  end
+
+  def show
+    render json: @entry
+  end
+
+  def create
+    @entry = Entry.create(entry_params)
+    render json: @entry, status: :created
+  end
+
+  def delete
+    # @entry.destroy
+    Entry.destroy(entry_params[user_id])
+  end
+
+  private
+
+  def entry_params
+    params.permit(:title, :content, :user_id)
+  end
+
+  def find_note
+    @entry = Entry.find(params[:id])
+  end
 end
